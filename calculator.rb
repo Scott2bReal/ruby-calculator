4c1e46efdf446afa2b07d6dd37dbae0417316c78
Scott Hoecker <hoeckers@gmail.com>
2021-07-31 14:38:31 -0500

    Implement basic functionality. Error handling needs work.

---
+++ calculator.rb
@@ -0,0 +1,40 @@
+def valid_number(number_string)
+  number_string.to_i.to_s == number_string && number_string.to_i != 0
+end
+
+def acquire_number
+  print('Please enter a number: ')
+  loop do
+    answer = gets.chomp
+    return answer if valid_number(answer)
+
+    print('Invalid input. Please enter a number: ')
+  end
+end
+
+def operate(num1, num2)
+  loop do
+    print('What would you like to do with these numbers? ')
+    operator = gets.chomp
+    begin
+      operation = "#{num1} #{operator} #{num2}"
+      puts "#{operation} = #{eval(operation)}"
+      break
+    rescue
+      puts "Sorry, I couldn't understand that."
+      next
+    end
+  end
+end
+
+loop do
+  puts "Welcome to Scott's Calculator!"
+
+  number1 = acquire_number
+  number2 = acquire_number
+
+  operate(number1, number2)
+  print('Press q to stop calculating (any key to continue)')
+  answer = gets.chomp
+  break if answer.downcase == 'q'
+end
